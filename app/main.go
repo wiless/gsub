e261ebb2bb6f558c6cb66bd8107914a6de767988
wiless <wiless.bytes@gmail.com>
2017-07-09 07:54:03 +0530

    minor changes for readability with comments

--- app/main.go
+++ app/main.go
@@ -28,16 +28,17 @@ func main() {
 	gsub.StartWebService("./web")
 	// tlogger := gusb.NewHttpLogger("Temperature")
 	var tlogger gsub.Logger
-	tlogger.Start("/monitor")
+	tlogger.Start("/monitor") // The /monitor url will print the stream of text coming from logger server or use a facncy html page ( /www/stream.html JS based event listener)
 	go gsub.StartServer()
 	queue := tlogger.Queue()
 
 	var lmsg gsub.LogMessage
 	for {
 		msg := fmt.Sprintf("I am a random number %d ", rand.Intn(100))
-		lmsg = gsub.LogMessage{Level: 1, Message: msg}
+		lmsg.Level = 1     // any number , for future use
+		lmsg.Message = msg // A string message to be broadcast to client, the server may append timestamps !!
 		queue <- lmsg
-		time.Sleep(1 * time.Second)
+		time.Sleep(2 * time.Second)
 	}
 
 	// GStop()
